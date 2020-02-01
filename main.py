153631481d66239ca582a3597786d94ec2ed86c3
Frozies <52686922+Frozies@users.noreply.github.com>
2020-01-31 21:43:17 -0500

    Updated main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 ###################
 # Checkbook Program
-# By Davin Young
+# Davin Young
+# Florida Gulf Coast University
+# Started Jan 2020
 ####################
 from datetime import datetime
 
@@ -18,6 +20,15 @@ def main():
                 print("No valid integer! Please try again ...")
         return n
 
+    def inputFloat(question): ##### Allows Float input that throws an error for exceptions
+        while True:
+            try:
+                f = float(input(question))
+                break
+            except ValueError:
+                print("No valid Float! Please try again ...")
+        return f
+
     def inputString(question): ##### Allows string input that throws an error for exceptions
         while True:
             try:
@@ -34,15 +45,17 @@ def main():
                 date_object = datetime.strptime(date_string, "%m/%d/%Y")
                 break
             except ValueError:
-                print("Incorrect format Use the format DD/MM/YYYY : ")
+                print("Incorrect format Use the format MM/DD/YYYY : ")
         return date_string
 
     ############################ Book Entry ############################
     def bookEntry():
         entryName = inputString("Please enter the entry's name: ")
-        entryDate = inputDate("Please enter the entry's date \nUse the format DD/MM/YYYY : ")
+        entryDate = inputDate("Please enter the entry's date \nUse the format MM/DD/YYYY : ")
+        entryAmount = inputFloat("Please enter an amount : $")
 
-        return entryName, entryDate
+        return entryName, entryDate, format(entryAmount, ",.2f")
+        ### Reformat the value to have a comma every 3 places, and 2 decimal points
 
     ############################ RUN ############################
     print("You entered:", bookEntry())
